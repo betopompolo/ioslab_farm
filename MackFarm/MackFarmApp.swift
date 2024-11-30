385e744f95fb4d5660b5ba6a73a89bf078a42609
Adalberto Pompolo <beto.nassu@gmail.com>
2024-11-30 12:14:21 -0300

    Initial Commit

---
+++ MackFarm/MackFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MackFarmApp.swift
+//  MackFarm
+//
+//  Created by Adalberto Nassu Pompolo on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MackFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MackFarmTests/MackFarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  MackFarmTests.swift
+//  MackFarmTests
+//
+//  Created by Adalberto Nassu Pompolo on 30/11/24.
+//
+
+import Testing
+@testable import MackFarm
+
+struct MackFarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
